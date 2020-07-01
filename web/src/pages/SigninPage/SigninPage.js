8885076a33a1d5f721ae4a6294bcdf37e1385038
Terris Kremer <terris.kremer@gmail.com>
2020-07-01 07:04:50 -0400

    final signin

--- web/src/pages/SigninPage/SigninPage.js
+++ web/src/pages/SigninPage/SigninPage.js
@@ -1,9 +1,26 @@
+// src/pages/SigninPage/SigninPage.js
+import { Form, TextField, PasswordField, Submit } from '@redwoodjs/web'
+import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
+
 const SigninPage = () => {
+  const { logIn } = useAuth()
+  const [error, setError] = React.useState(null)
+
+  const onSubmit = (data) => {
+    setError(null)
+    logIn({ email: data.email, password: data.password, remember: true })
+      .then(() => navigate(routes.home()))
+      .catch((error) => setError(error.message))
+  }
+
   return (
-    <div>
-      <h1>SigninPage</h1>
-      <p>Find me in ./web/src/pages/SigninPage/SigninPage.js</p>
-    </div>
+    <Form onSubmit={onSubmit}>
+      {error && <p>{error}</p>}
+      <TextField name="email" placeholder="email" />
+      <PasswordField name="password" placeholder="password" />
+      <Submit>Sign In</Submit>
+    </Form>
   )
 }
 
