a96535029f98c8c5522cfadcfc8397cc9ccf0a85
Terris Kremer <terris.kremer@gmail.com>
2020-07-01 07:12:57 -0400

    final signout

---
+++ web/src/components/SignoutBtn/SignoutBtn.js
@@ -0,0 +1,15 @@
+// web/src/components/SignoutBtn/SignoutBtn.js
+import { useAuth } from '@redwoodjs/auth'
+import { navigate, routes } from '@redwoodjs/router'
+
+const SignoutBtn = () => {
+  const { logOut } = useAuth()
+
+  const onClick = () => {
+    logOut().then(() => navigate(routes.home()))
+  }
+
+  return <button onClick={() => onClick()}>Sign Out</button>
+}
+
+export default SignoutBtn
